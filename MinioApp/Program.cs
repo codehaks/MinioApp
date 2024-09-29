31340f7ad53608ff1889c01e88c501ec2a06d6c2
Hakim <ghods.hakim@hotmail.com>
2024-09-29 17:34:37 +0330

    Add project files.

---
+++ MinioApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Minio;
+using Minio.DataModel.Args;
+
+Console.WriteLine("Hello, World!");
+
+// MinIO server credentials
+string endpoint = "localhost:9000"; // Your MinIO server endpoint
+string accessKey = "minioadmin"; // Your access key
+string secretKey = "minioadmin"; // Your secret key
+
+var minioClient = new MinioClient()
+    .WithEndpoint(endpoint)
+    .WithCredentials(accessKey, secretKey)
+    //.WithSSL()
+    .Build();
+
+var bucketList = await minioClient.ListBucketsAsync();
+
+foreach (var bucket in bucketList.Buckets)
+{
+    Console.WriteLine(bucket.Name);
+    var objArgs = new ListObjectsArgs().WithBucket(bucket.Name);
+    var objList = minioClient.ListObjectsEnumAsync(objArgs);
+    await foreach (var item in objList)
+    {
+        if (item.IsDir)
+        {
+            continue;
+        }
+        Console.WriteLine($" -- {item.Key}   |{item.Size/1024} KB");
+        
+    }
+}
+
